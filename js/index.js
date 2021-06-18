0896b355601bd9d6f8920b3594e2ba6e272a3d55
fsy654910915 <654910915@qq.com>
2021-06-18 20:22:33 +0800

    修改样式

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 $(function(){
-    $el = $('.col-md-2 a img');
+    $el = $('.col-md-2');
+    console.log($el);
     $el.hover(function(){
-        $(this).next('.xiala').css({top:'0'});
+        $(this).find('.xiala').css({top:0});
     },function(){
-        $(this).next('.xiala').css({top:'-100%'});
+        $(this).find('.xiala').css({top:'-100%'});
     });
-    $(this).next('.xiala').hover(function(){
-        $(this).css({top:'0'});
-    },function(){
-        $(this).css({top:'-100%'});
-    });
+    // $(this).next('.xiala').hover(function(){
+    //     $(this).css({top:'0'});
+    // },function(){
+    //     $(this).css({top:'-100%'});
+    // });
 })
